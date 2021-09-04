4b5f4b99404b817bc68301b71591630774b59785
Suganya Panneerselvam <suganya.panneerselvam@dataquest.com.au>
2021-09-04 20:24:55 +0530

    GetValue property

--- Reflection/002_PropertyInfo/Program.cs
+++ Reflection/002_PropertyInfo/Program.cs
@@ -15,6 +15,31 @@ namespace _002_PropertyInfo
             PrintAllProperties<string>();
             PrintAllProperties<List<int>>();
             PrintAllProperties<Queue<int>>();
+            //-----------------------------------------------//
+            Console.WriteLine("-----------------------------------------------");
+            Student student1 = new Student()
+            {
+                Name="sugu",
+                Age=22,
+            };
+            Student student2 = new Student()
+            {
+                Name = "venkat",
+                Age = 25,
+            };
+            PrintAllPropertiesWithValue<Student>(student1);
+            PrintAllPropertiesWithValue<Student>(student2);
+            Teacher teacher = new Teacher()
+            {
+                Name = "sagu",
+                Age = 42,
+                Subject="Maths",
+            };
+            PrintAllPropertiesWithValue<Teacher>(teacher);
+            //-----------------------------------------------//
+            Console.WriteLine("-----------------------------------------------");
+            PrintPropertyWithValue<Student>(student1,"Age");
+            PrintPropertyWithValue<Teacher>(teacher, "subject");
         }
         public static void PrintAllProperties<T>()
         {
@@ -24,6 +49,28 @@ namespace _002_PropertyInfo
             foreach (var item in properties)
             {
                 Console.WriteLine(item);
+            }
+        }
+        public static void PrintAllPropertiesWithValue<T>(T obj)
+        {
+            var type=obj.GetType();
+            var props = type.GetProperties();
+            foreach (var prop in props)
+            {
+                Console.WriteLine(prop+" "+prop.GetValue(obj));
+            }
+        }
+        public static void PrintPropertyWithValue<T>(T obj,string propName)
+        {
+            var type = obj.GetType();
+            var prop = type.GetProperty(propName);
+            if (prop!=null)
+            {
+                Console.WriteLine(prop + " " + prop.GetValue(obj));
+            }
+            else
+            {
+                Console.WriteLine("property is not found");
             }
         }
     }
