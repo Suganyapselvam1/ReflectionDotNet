83987ac5d515b859ae990bd035fc83c50bd11f86
Suganya Panneerselvam <suganya.panneerselvam@dataquest.com.au>
2021-11-18 13:12:28 +0530

    Assigned set value for property

---
+++ Reflection/SetValue/DynamicRoleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetValue
+{
+    public class DynamicRoleModel
+    {
+        public bool MangeMember { get; set; }
+        public bool  AdminMember { get; set; }
+        public bool  SuiteMember { get; set; }
+        public bool PackageMember { get; set; }
+    }
+}
---
+++ Reflection/SetValue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicRoleModel dynamicRoleModel = new DynamicRoleModel();
+            string[] newPermissions = { "mangeMember", "AdminMember" };
+            SetPropertyWithValue(dynamicRoleModel, newPermissions);
+        }
+        public static void SetPropertyWithValue<T>(T obj, string[] newPermissions)
+        {
+            foreach (var item in newPermissions)
+            {
+                var type = obj.GetType();
+                var props = type.GetProperties();
+
+                var prop = props.FirstOrDefault(x =>x.Name.Equals(item,StringComparison.OrdinalIgnoreCase));
+                if (prop!=null)
+                {
+                    var d=prop.GetValue(obj);
+                    prop.SetValue(obj, true);
+                    var g = prop.GetValue(obj);
+                }
+            }
+        }
+    }
+}
